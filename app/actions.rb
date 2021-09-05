3724585706819dca4a89c4586da5a58276b8783b
Arjun Chowdhury <arjun@ualberta.ca>
2021-09-05 22:18:14 +0000

    W1D1 setup

--- app/actions.rb
+++ app/actions.rb
@@ -5,3 +5,47 @@ get '/' do
   erb(:index)
 
 end
+
+
+get '/signup' do
+
+  @user = User.new
+
+  erb(:signup)
+
+end
+
+post '/signup' do
+
+  # grab user input values from params
+  
+  email      = params[:email]
+  
+  avatar_url = params[:avatar_url]
+  
+  username   = params[:username]
+  
+  password   = params[:password]
+
+    # instantiate and save a User
+  
+  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+  
+  # if user validations pass and user is saved
+
+  if @user.save
+
+  "User #{username} saved!"
+  
+  else
+
+    erb(:signup)
+
+  end
+
+end
+
+
+
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ has_many :comments
 
 has_many :likes
 
+validates :email, :username, uniqueness: true
+
+validates :email, :avatar_url, :username, :password, presence: true
+
 end
